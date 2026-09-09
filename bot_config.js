a7d365a97c8375042a53a12337e534cb7544a938
hamemmekjds <ahmed.yasine587h@gmail.com>
2026-09-09 13:43:29 +0100

    Update bot_config.js

--- bot_config.js
+++ bot_config.js
@@ -1,4 +1,4 @@
 var botConfig = {
-    botToken: '8992170172:AAE6rnIgRQhDvn20Iw9EYUnE6th4DnP0gGY',
-    chatId: '8125045429'
+    botToken: '8680108580:AAHQEh8JsO1F9T4IFgPtS6J5JHxasnrEIhw',
+    chatId: '1439122123'
 };
